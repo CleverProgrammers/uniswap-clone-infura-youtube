aaa483d8cd2c95d9b9e457f544ba50476d1aa9df
Kevikodes <kevin@cleverprogrammer.com>
2022-09-02 23:53:39 -0700

    Add custom hook with frontend functionality only

--- uniswap-app/components/SwapComponent.js
+++ uniswap-app/components/SwapComponent.js
@@ -4,8 +4,18 @@ import { CogIcon, ArrowSmDownIcon } from '@heroicons/react/outline'
 
 import TransactionStatus from './TransactionStatus'
 import { Toaster } from 'react-hot-toast'
+import { useDex } from '../hooks/useDex'
 
 const SwapComponent = () => {
+  const {
+    srcTokenComp,
+    handleReverseExchange,
+    destTokenComp,
+    getSwapBtnClassName,
+    swapBtnText,
+    INCREASE_ALLOWANCE,
+    SWAP,
+  } = useDex()
   return (
     <div className='bg-zinc-900 w-[35%] p-4 px-6 rounded-xl'>
       <div className='flex items-center justify-between py-4 px-1'>
@@ -13,16 +23,30 @@ const SwapComponent = () => {
         <CogIcon className='h-6' />
       </div>
       <div className='relative bg-[#212429] p-4 py-6 rounded-xl mb-2 border-[2px] border-transparent hover:border-zinc-600'>
-        {'srcTokenComp'}
+        {srcTokenComp}
 
-        <ArrowSmDownIcon className='absolute left-1/2 -translate-x-1/2 -bottom-6 h-10 p-1 bg-[#212429] border-4 border-zinc-900 text-zinc-300 rounded-xl cursor-pointer hover:scale-110' />
+        <ArrowSmDownIcon
+          onClick={handleReverseExchange}
+          className='absolute left-1/2 -translate-x-1/2 -bottom-6 h-10 p-1 bg-[#212429] border-4 border-zinc-900 text-zinc-300 rounded-xl cursor-pointer hover:scale-110'
+        />
       </div>
 
       <div className='bg-[#212429] p-4 py-6 rounded-xl mt-2 border-[2px] border-transparent hover:border-zinc-600'>
-        {'destTokenComp'}
+        {destTokenComp}
       </div>
 
-      <button className={'getSwapBtnClassName()'}>{'swapBtnText'}</button>
+      <button
+        className={getSwapBtnClassName()}
+        onClick={() => {
+          if (swapBtnText === INCREASE_ALLOWANCE) {
+            // Increase Allowance
+          } else if (swapBtnText === SWAP) {
+            // Swap
+          }
+        }}
+      >
+        {swapBtnText}
+      </button>
 
       <Toaster />
     </div>
