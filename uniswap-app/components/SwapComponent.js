d51cfd8b5f10e3757dc97aa87e553621ddf37628
Kevikodes <kevin@cleverprogrammer.com>
2022-09-03 17:19:10 -0700

    new cli

--- uniswap-app/components/SwapComponent.js
+++ uniswap-app/components/SwapComponent.js
@@ -1,21 +1,101 @@
 import React, { useEffect, useState, useRef } from 'react'
+import {
+  hasValidAllowance,
+  increaseAllowance,
+  swapEthToToken,
+  swapTokenToEth,
+  swapTokenToToken,
+} from '../utils/queries'
 
 import { CogIcon, ArrowSmDownIcon } from '@heroicons/react/outline'
-
+import SwapField from './SwapField'
 import TransactionStatus from './TransactionStatus'
-import { Toaster } from 'react-hot-toast'
-import { useDex } from '../hooks/useDex'
+import toast, { Toaster } from 'react-hot-toast'
+import { DEFAULT_VALUE, ETH } from '../utils/SupportedCoins'
+import { toEth, toWei } from '../utils/ether-utils'
+import { useAccount } from 'wagmi'
 
 const SwapComponent = () => {
-  const {
-    srcTokenComp,
-    handleReverseExchange,
-    destTokenComp,
-    getSwapBtnClassName,
-    swapBtnText,
-    INCREASE_ALLOWANCE,
-    SWAP,
-  } = useDex()
+  const [srcToken, setSrcToken] = useState(ETH)
+  const [destToken, setDestToken] = useState(DEFAULT_VALUE)
+
+  const [inputValue, setInputValue] = useState()
+  const [outputValue, setOutputValue] = useState()
+
+  const inputValueRef = useRef()
+  const outputValueRef = useRef()
+
+  const isReversed = useRef(false)
+
+  const INCREASE_ALLOWANCE = 'Increase allowance'
+  const ENTER_AMOUNT = 'Enter an amount'
+  const CONNECT_WALLET = 'Connect wallet'
+  const SWAP = 'Swap'
+
+  const srcTokenObj = {
+    id: 'srcToken',
+    value: inputValue,
+    setValue: setInputValue,
+    defaultValue: srcToken,
+    ignoreValue: destToken,
+    setToken: setSrcToken,
+  }
+
+  const destTokenObj = {
+    id: 'destToken',
+    value: outputValue,
+    setValue: setOutputValue,
+    defaultValue: destToken,
+    ignoreValue: srcToken,
+    setToken: setDestToken,
+  }
+
+  const [srcTokenComp, setSrcTokenComp] = useState()
+  const [destTokenComp, setDestTokenComp] = useState()
+
+  const [swapBtnText, setSwapBtnText] = useState(ENTER_AMOUNT)
+  const [txPending, setTxPending] = useState(false)
+
+  const notifyError = msg => toast.error(msg, { duration: 6000 })
+  const notifySuccess = () => toast.success('Transaction completed.')
+  const address = ''
+  useEffect(() => {
+    // Handling the text of the submit button
+
+    if (!address) setSwapBtnText(CONNECT_WALLET)
+    else if (!inputValue || !outputValue) setSwapBtnText(ENTER_AMOUNT)
+    else setSwapBtnText(SWAP)
+  }, [inputValue, outputValue, address])
+
+  useEffect(() => {
+    if (
+      document.activeElement !== outputValueRef.current &&
+      document.activeElement.ariaLabel !== 'srcToken' &&
+      !isReversed.current
+    )
+      populateOutputValue(inputValue)
+
+    setSrcTokenComp(<SwapField obj={srcTokenObj} ref={inputValueRef} />)
+
+    if (inputValue?.length === 0) setOutputValue('')
+  }, [inputValue, destToken])
+
+  useEffect(() => {
+    if (
+      document.activeElement !== inputValueRef.current &&
+      document.activeElement.ariaLabel !== 'destToken' &&
+      !isReversed.current
+    )
+      populateInputValue(outputValue)
+
+    setDestTokenComp(<SwapField obj={destTokenObj} ref={outputValueRef} />)
+
+    if (outputValue?.length === 0) setInputValue('')
+
+    // Resetting the isReversed value if its set
+    if (isReversed.current) isReversed.current = false
+  }, [outputValue, srcToken])
+
   return (
     <div className='bg-zinc-900 w-[35%] p-4 px-6 rounded-xl'>
       <div className='flex items-center justify-between py-4 px-1'>
@@ -26,8 +106,8 @@ const SwapComponent = () => {
         {srcTokenComp}
 
         <ArrowSmDownIcon
-          onClick={handleReverseExchange}
           className='absolute left-1/2 -translate-x-1/2 -bottom-6 h-10 p-1 bg-[#212429] border-4 border-zinc-900 text-zinc-300 rounded-xl cursor-pointer hover:scale-110'
+          onClick={handleReverseExchange}
         />
       </div>
 
@@ -38,19 +118,104 @@ const SwapComponent = () => {
       <button
         className={getSwapBtnClassName()}
         onClick={() => {
-          if (swapBtnText === INCREASE_ALLOWANCE) {
-            // Increase Allowance
-          } else if (swapBtnText === SWAP) {
-            // Swap
-          }
+          if (swapBtnText === INCREASE_ALLOWANCE) handleIncreaseAllowance()
+          else if (swapBtnText === SWAP) handleSwap()
         }}
       >
         {swapBtnText}
       </button>
 
+      {txPending && <TransactionStatus />}
+
       <Toaster />
     </div>
   )
+
+  async function handleIncreaseAllowance() {
+    // Increase the allowance
+    setTxPending(true)
+    await increaseAllowance(srcToken, inputValue)
+    setTxPending(false)
+
+    // Set the swapbtn to "Swap" again
+    setSwapBtnText(SWAP)
+  }
+
+  function handleReverseExchange(e) {
+    // Setting the isReversed value to prevent the input/output values
+    // being calculated in their respective side - effects
+    isReversed.current = true
+
+    // 1. Swap tokens (srcToken <-> destToken)
+    // 2. Swap values (inputValue <-> outputValue)
+
+    setInputValue(outputValue)
+    setOutputValue(inputValue)
+
+    setSrcToken(destToken)
+    setDestToken(srcToken)
+  }
+
+  function getSwapBtnClassName() {
+    let className = 'p-4 w-full my-2 rounded-xl'
+    className +=
+      swapBtnText === ENTER_AMOUNT || swapBtnText === CONNECT_WALLET
+        ? ' text-zinc-400 bg-zinc-800 pointer-events-none'
+        : ' bg-blue-700'
+    className += swapBtnText === INCREASE_ALLOWANCE ? ' bg-yellow-600' : ''
+    return className
+  }
+
+  function populateOutputValue() {
+    if (
+      destToken === DEFAULT_VALUE ||
+      srcToken === DEFAULT_VALUE ||
+      !inputValue
+    )
+      return
+
+    try {
+      if (srcToken !== ETH && destToken !== ETH) setOutputValue(inputValue)
+      else if (srcToken === ETH && destToken !== ETH) {
+        const outValue = toEth(toWei(inputValue), 14)
+        setOutputValue(outValue)
+      } else if (srcToken !== ETH && destToken === ETH) {
+        const outValue = toEth(toWei(inputValue, 14))
+        setOutputValue(outValue)
+      }
+    } catch (error) {
+      setOutputValue('0')
+    }
+  }
+
+  function populateInputValue() {
+    if (
+      destToken === DEFAULT_VALUE ||
+      srcToken === DEFAULT_VALUE ||
+      !outputValue
+    )
+      return
+
+    try {
+      if (srcToken !== ETH && destToken !== ETH) setInputValue(outputValue)
+      else if (srcToken === ETH && destToken !== ETH) {
+        const outValue = toEth(toWei(outputValue, 14))
+        setInputValue(outValue)
+      } else if (srcToken !== ETH && destToken === ETH) {
+        const outValue = toEth(toWei(outputValue), 14)
+        setInputValue(outValue)
+      }
+    } catch (error) {
+      setInputValue('0')
+    }
+  }
+
+  function handleInsufficientAllowance() {
+    notifyError(
+      "Insufficient allowance. Click 'Increase allowance' to increase it.",
+    )
+    setSwapBtnText(INCREASE_ALLOWANCE)
+  }
 }
 
 export default SwapComponent
